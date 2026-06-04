1f96d59cb4a70ca53a03549a5d350d468942d95e
Jacob Flentke <jakeflentke@gmail.com>
2026-06-03 17:35:15 -0700

    DDL Script

--- Lessons/1.21_DDL_&_DML_PT1.sql
+++ Lessons/1.21_DDL_&_DML_PT1.sql
@@ -1,26 +1,35 @@
+--.read Lessons/1.21_DDL_&_DML_PT1.sql
+
+USE data_jobs;
+
+DROP DATABASE IF EXISTS jobs_mart;
+
+CREATE DATABASE IF NOT EXISTS jobs_mart;
+
 SHOW DATABASES;
 
-CREATE DATABASE jobs_mart;
-
-SELECT 
-    catalog_name,
-    schema_name
+SELECT *
 FROM information_schema.schemata;
 
 USE jobs_mart;
 
 CREATE SCHEMA IF NOT EXISTS staging;
 
-CREATE TABLE IF NOT EXISTS staging.priority_roles
+CREATE TABLE IF NOT EXISTS jobs_mart.staging.preferred_roles
     (
-        role_id INT,
+        role_id INT PRIMARY KEY,
         role_name VARCHAR
     );
 
-SELECT 
-    table_catalog,
-    table_name,
-    table_schema
+SELECT *
 FROM information_schema.tables
 WHERE table_catalog = 'jobs_mart';
 
+INSERT INTO jobs_mart.staging.preferred_roles (role_id, role_name)
+VALUES
+    (1, 'Data Engineer'),
+    (2, 'Senior Data Engineer'),
+    (3, 'Software Engineer');
+
+SELECT * 
+FROM jobs_mart.staging.preferred_roles;
